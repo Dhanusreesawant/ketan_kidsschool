e9c43c20cec7b9c2ec7055c2da3e8a6cbf5329a4
Dhanusreesawant <sawantdhanusree5@gmail.com>
2025-08-05 12:14:32 +0530

    Initial Project commit

---
+++ assets/js/script.js
@@ -0,0 +1,83 @@
+const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 10,
+    mousewheel: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        680: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        992: {
+            slidesPerView: 3
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+});
+
+document.getElementById('header').innerHTML = fetch('header.html').then(res => res.text());
+
+// Odometer JS
+	$('.odometer').appear(function(e) {
+		var odo = $(".odometer");
+		odo.each(function() {
+			var countNumber = $(this).attr("data-count");
+			$(this).html(countNumber);
+		});
+        setTimeout(() => {
+      odometerElement.innerHTML = $(this).attr("data-count"); ; // Replace with your desired number
+    }, 1000);
+	});
+    
+//Faq
+$(function() {
+		$('.accordion').find('.accordion-title').on('click', function(){
+			// Adds Active Class
+			$(this).toggleClass('active');
+			// Expand or Collapse This Panel
+			$(this).next().slideToggle('fast');
+			// Hide The Other Panels
+			$('.accordion-content').not($(this).next()).slideUp('fast');
+			// Removes Active Class From Other Titles
+			$('.accordion-title').not($(this)).removeClass('active');		
+		});
+	});
+
+//skills
+jQuery('.skill-bar').each(function() {
+		jQuery(this).find('.progress-content, .progress-content-two, .progress-content-three').animate({
+		width:jQuery(this).attr('data-percentage')
+		},2000);
+		
+		jQuery(this).find('.progress-number-mark').animate(
+		{left:jQuery(this).attr('data-percentage')},
+		{
+			duration: 2000,
+			step: function(now, fx) {
+			var data = Math.round(now);
+			jQuery(this).find('.percent').html(data + '%');
+			}
+		});  
+	});
